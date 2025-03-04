8b4bdec63e4ee5c59578f7048edeb8c35998f113
mphhohku <130424415+mphhohku@users.noreply.github.com>
2025-03-04 11:48:53 +0800

    [test] add additional test cases for reverseString function

--- exercise.test.js
+++ exercise.test.js
@@ -3,4 +3,10 @@ import { reverseString } from './exercise.js';
 test('reverseString reverses the string', () => {
     expect(reverseString('hello')).toBe('olleh');
     // add more tests here
+    expect(reverseString('')).toBe('');
+    expect(reverseString('a')).toBe('a');
+    expect(reverseString('ab')).toBe('ba');
+    expect(reverseString('abc')).toBe('cba');
+    expect(reverseString('abcd')).toBe('dcba');
+    
 });
